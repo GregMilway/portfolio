9e3fd082e64592848dbf66a1df6f75c25dc88171
greg <greg.milway@gmail.com>
2024-08-21 00:05:18 +0100

    "Added initial version of jobs app, including bootstrap and popper.js Added isort to project to clean imports"

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+import jobs.views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", jobs.views.home, name="home"),
+    path("jobs/<int:job_id>", jobs.views.detail, name="detail"),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
